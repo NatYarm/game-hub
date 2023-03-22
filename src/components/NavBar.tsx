66e9ae22e9bfdee63148ef994dc530fd48ee6987
NatYarm <yarmnat7@gmail.com>
2023-03-22 16:14:38 +0300

    Show a spinner while fetching the genres

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -4,7 +4,7 @@ import ColorModeSwitch from './ColorModeSwitch';
 
 const NavBar = () => {
   return (
-    <HStack justifyContent="space-between" paddingX={3}>
+    <HStack justifyContent="space-between" paddingX={3} marginBottom={3}>
       <Image src={logo} boxSize="60px" />
       <ColorModeSwitch />
     </HStack>
